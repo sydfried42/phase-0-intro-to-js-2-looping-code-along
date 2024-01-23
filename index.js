65caae14a41675f15a8291d0382d8bb7d4f72fe2
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-01-22 22:57:34 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+
+
+const namesArray = ["Bex", "Daniel", "Sydney"];
+const eventName = "Christmas"; 
+
+function writeCards(names, event) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+  }
+
+  return messages;
+}
+
+const result = writeCards(namesArray, eventName);
+console.log(result);
+
+
+function countDown(startingNumber) {
+    let currentNumber = startingNumber;
+  
+    while (currentNumber >= 0) {
+      console.log(currentNumber);
+      currentNumber--;
+    }
+  }
+  
+  countDown(10);
